33c79ceb66a084ea5b7cc5e3f74c0fa017b43a13
prabakaran <mspk028@gmail.com>
2023-02-04 14:06:34 +0530

    desktop edited

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 // elements
-const navEl = document.getElementById("nav");
+const navEl = document.querySelector(".hero-container");
 const barIconEl = document.querySelector(".menu-btn i");
 const listEl = document.querySelectorAll("nav ul li");
+const imageEl = document.querySelectorAll(".image-collections-desktop div");
+const seeAllBtn = document.getElementById("btn");
 
 // eventlistneres
 barIconEl.addEventListener("click", () => {
@@ -13,5 +15,11 @@ listEl.forEach((val) => {
     let activeLi = document.querySelector("nav ul li.active");
     activeLi.classList.remove("active");
     val.classList.add("active");
+  });
+});
+
+seeAllBtn.addEventListener("click", () => {
+  imageEl.forEach((img) => {
+    img.classList.toggle("show");
   });
 });
